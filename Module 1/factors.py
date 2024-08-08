3432614347ed1be8e2ba243726218f662bb4efb3
g1gasad <asadahsan2245@gmail.com>
2024-08-08 22:57:46 +0530

    factors

--- Module 1/factors.py
+++ Module 1/factors.py
@@ -1,10 +1,13 @@
 # 10/2==0
 def factors(num):
-    fac_list = [1]
-    for i in range(2, num-1):
-        if num%i==0:
-            fac_list.append(i)
-    return fac_list            
+    if num < 1:
+        return "Not a valid input"
+    else:
+        fac_list = [1]
+        for i in range(2, num):
+            if num%i==0:
+                fac_list.append(i)
+        return fac_list            
 num = int(input("Enter a natural number: "))
 
 print(factors(num))
