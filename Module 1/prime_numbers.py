cf90fd1f3646d82fcff45e67390d5314187cbaf8
g1gasad <asadahsan2245@gmail.com>
2024-08-08 22:54:59 +0530

    primes

--- Module 1/prime_numbers.py
+++ Module 1/prime_numbers.py
@@ -1,6 +1,5 @@
 def checkPrimes(n):
-    for i in range(2, n):
-        
+    for i in range(2, n+1):
         for j in range(2, i):
             if i%j==0:
                 print(f"{i}: Not Prime")
@@ -8,4 +7,4 @@ def checkPrimes(n):
         else:
             print(f"{i}: Prime")
     
-checkPrimes(10)
+checkPrimes(20)
